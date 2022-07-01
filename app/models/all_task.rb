cdf9a218f82274cfbade521c593fb7b122b01aea
Alberto Quezada <drinksomewater17@gmail.com>
2022-07-01 09:57:30 -0700

    Fixed completed/uncompleted buttons- updated to not sure created at when creating task, will automatically do it

--- app/models/all_task.rb
+++ app/models/all_task.rb
@@ -3,4 +3,8 @@ class AllTask < ApplicationRecord
     def self.search(search)
         where("lower(alltask.task_title) LIKE :search ", search: "%#{search.downcase}%").uniq
     end
+
+    #Validation when posting data to database
+    validates :task_title, presence: true 
+    #validates :task_due_date, presence: true
 end
